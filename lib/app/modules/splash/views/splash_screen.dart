a9b36f4d7689a0063ca79a7e8fac84f962384fa7
shoaib <shoaib3974@gmail.com>
2025-10-14 01:15:33 +0500

    add loading in splash screen

--- lib/app/modules/splash/views/splash_screen.dart
+++ lib/app/modules/splash/views/splash_screen.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:shopperz/config/routes/app_routes.dart';
 import 'package:shopperz/utils/svg_icon.dart';
-
 import '../../../../config/theme/app_color.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -18,8 +17,9 @@ class SplashScreen extends StatelessWidget {
     Timer(const Duration(seconds: 3), () {
       Get.offNamed(Routes.navBarView);
     });
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
-      value:const SystemUiOverlayStyle(
+      value: const SystemUiOverlayStyle(
         systemNavigationBarColor: Colors.white,
         systemNavigationBarIconBrightness: Brightness.dark,
         statusBarIconBrightness: Brightness.dark,
@@ -29,9 +29,22 @@ class SplashScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: AppColor.primaryBackgroundColor,
         body: Center(
-          child: SvgPicture.asset(
-            SvgIcon.logo,
-            height: 50.h,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // App Logo
+              SvgPicture.asset(
+                SvgIcon.logo,
+                height: 50.h,
+              ),
+              SizedBox(height: 30.h),
+
+              // Dots Loader
+              const SpinKitThreeBounce(
+                color: Colors.white, 
+                size: 20.0,
+              ),
+            ],
           ),
         ),
       ),
